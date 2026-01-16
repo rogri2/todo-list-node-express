643f7afe00b99bbfad8a4cf243286cca7b99f286
Rodrigo Yap <rodyap182@gmail.com>
2026-01-15 20:47:02 -0600

    Eliminacion de linea en blanco

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public/home')));
 app.use('/api/tasks', taskRoutes);
 
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
