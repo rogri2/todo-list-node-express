ba68cc411e0522da909f3d58e40a1b9bc0b8a953
Rodrigo Yap <rodyap182@gmail.com>
2026-01-15 20:31:13 -0600

    Correccion de modelo

--- models/task.model.js
+++ models/task.model.js
@@ -1,9 +1,15 @@
 class Task {
-  constructor(id, title, description, completed = false) {
+  constructor({ id, title, description, completed = false }) {
+    if (!title) {
+      throw new Error('Titulo obligatorio');
+    }
     this.id = id;
     this.title = title;
     this.description = description;
     this.completed = completed;
+  }
+  toggleComplete() {
+    this.completed = !this.completed;
   }
 }
 
